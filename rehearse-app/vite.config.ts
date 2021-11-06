65b3f1493b02dd8919426aa7c28dd5907ec06a9f
Bluskript <bluskript@gmail.com>
2021-11-06 16:02:23 -0400

    epic midi working and midi reading

--- rehearse-app/vite.config.ts
+++ rehearse-app/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
+import mkcert from'vite-plugin-mkcert'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
+  server: {
+    https: true
+  },
   plugins: [
     WindiCSS(),
+    mkcert(),
     vue(),
   ]
 })
