dcc03612109b9cfdd3130fcd4e1e3f61bcdc70ac
Rohan Dugad <rohand2468@gmail.com>
2021-11-06 18:32:34 -0400

    Combined both hands and midi output

--- rehearse-teensy/Final/src/main.cpp
+++ rehearse-teensy/Final/src/main.cpp
@@ -1,9 +1,45 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const int channel = 1;
+
+const uint8_t lThumb = A1;
+const uint8_t lIndex = A0;
+const uint8_t lMiddle = A2;
+const uint8_t lRing = A3;
+const uint8_t lPinky = A4;
+const uint8_t rThumb = A5;
+const uint8_t rIndex = A7;
+const uint8_t rMiddle = A8;
+const uint8_t rRing = A9;
+const uint8_t rPinky = A6;
+
+const int FINGERS = 10;
+const uint8_t fingers[FINGERS] = {lThumb, lIndex, lMiddle, lRing, lPinky,
+                                  rThumb, rIndex, rMiddle, rRing, rPinky};
+
+void updateFinger(uint8_t analogPin, uint8_t note)
+{
+    int read = analogRead(analogPin);
+    if (read > 900)
+    {
+        usbMIDI.sendNoteOn(note, read, channel);
+    }
+    else
+    {
+        usbMIDI.sendNoteOff(note, 0, channel);
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    for (int i = 0; i < FINGERS; i++) {
+        updateFinger(fingers[i], i);
+    }
+
+    while (usbMIDI.read());
 }
