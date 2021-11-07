42e7111cce0400dc3d203ef87611194d146981dd
Rohan Dugad <rohand2468@gmail.com>
2021-11-06 21:34:15 -0400

    Tested and mapped right hand LEDs

--- rehearse-teensy/LED-Test/src/main.cpp
+++ rehearse-teensy/LED-Test/src/main.cpp
@@ -1,17 +1,27 @@
 #include <Arduino.h>
 
+const uint8_t rThumbLed = 1;
+const uint8_t rIndexLed = 4;
+const uint8_t rMiddleLed = 2;
+const uint8_t rRingLed = 3;
+const uint8_t rPinkyLed = 0;
+
 const int LED_NUM = 5;
+const uint8_t ledPins[] = {rThumbLed, rIndexLed, rMiddleLed, rRingLed, rPinkyLed};
 
 void setup()
 {
     for (int i = 0; i < LED_NUM; i++) {
-        pinMode(i, OUTPUT);
+        pinMode(ledPins[i], OUTPUT);
     }
 }
 
 void loop()
 {
     for (int i = 0; i < LED_NUM; i++) {
-        digitalWrite(i, HIGH);
+        digitalWrite(ledPins[i], HIGH);
+        delay(2000);
+        digitalWrite(ledPins[i], LOW);
+        delay(2000);
     }
 }
