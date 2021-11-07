55c20a62a7693083bd7d705ee74f8365de33c122
Rohan Dugad <rohand2468@gmail.com>
2021-11-07 07:18:05 -0500

    Tested left hand LEDs

--- rehearse-teensy/LED-Test/src/main.cpp
+++ rehearse-teensy/LED-Test/src/main.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 
+const uint8_t lThumbLed = 5;
+const uint8_t lIndexLed = 6;
+const uint8_t lMiddleLed = 7;
+const uint8_t lRingLed = 8;
+const uint8_t lPinkyLed = 9;
 const uint8_t rThumbLed = 1;
 const uint8_t rIndexLed = 4;
 const uint8_t rMiddleLed = 2;
 const uint8_t rRingLed = 3;
 const uint8_t rPinkyLed = 0;
 
-const int LED_NUM = 5;
-const uint8_t ledPins[] = {rThumbLed, rIndexLed, rMiddleLed, rRingLed, rPinkyLed};
+const int LED_NUM = 10;
+const uint8_t ledPins[LED_NUM] = {lThumbLed, lIndexLed, lMiddleLed, lRingLed, lPinkyLed,
+                                  rThumbLed, rIndexLed, rMiddleLed, rRingLed, rPinkyLed};
 
 void setup()
 {
