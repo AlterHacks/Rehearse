97d7f7d9addda0084b5d872b25706677865aa3ec
Rohan Dugad <rohand2468@gmail.com>
2021-11-07 07:20:02 -0500

    Adapted IMU test code from rcj project

--- rehearse-teensy/IMU-Test/src/main.cpp
+++ rehearse-teensy/IMU-Test/src/main.cpp
@@ -1,9 +1,42 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <IMU_Wrapper.h>
 
-void setup() {
-  // put your setup code here, to run once:
+IMU_Wrapper myImu(100);
+
+adafruit_bno055_offsets_t offsets{-34, 51, -25,
+                                  3, -1, 3,
+                                  -98, 11, 318,
+                                  1000, 722};
+
+/**************************************************************************/
+/*
+    Arduino setup function (automatically called at startup)
+*/
+/**************************************************************************/
+void setup(void)
+{
+    Serial.begin(115200);
+
+    myImu.setOffsets(offsets);
+    myImu.begin();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+/**************************************************************************/
+/*
+    Arduino loop function, called once 'setup' is complete (your own code
+    should go here)
+*/
+/**************************************************************************/
+void loop(void)
+{
+    /* Get a new sensor information */
+    myImu.update();
+
+    Serial.print(myImu.getYaw());
+    Serial.print("\t");
+    Serial.print(myImu.getPitch());
+    Serial.print("\t");
+    Serial.print(myImu.getRoll());
+    Serial.print("\n");
 }
