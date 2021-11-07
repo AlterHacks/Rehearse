0d63de1cf0a48aa69b6502c24a459fced5701fd3
Rohan Dugad <rohand2468@gmail.com>
2021-11-07 07:17:32 -0500

    Tested left hand pressure sensors

--- rehearse-teensy/Pressure-Test/src/main.cpp
+++ rehearse-teensy/Pressure-Test/src/main.cpp
@@ -1,10 +1,15 @@
 #include <Arduino.h>
 
-const uint8_t rThumb = A1;
-const uint8_t rIndex = A0;
-const uint8_t rMiddle = A2;
-const uint8_t rRing = A3;
-const uint8_t rPinky = A4;
+const uint8_t lThumb = A1;
+const uint8_t lIndex = A0;
+const uint8_t lMiddle = A2;
+const uint8_t lRing = A3;
+const uint8_t lPinky = A4;
+const uint8_t rThumb = A5;
+const uint8_t rIndex = A7;
+const uint8_t rMiddle = A8;
+const uint8_t rRing = A9;
+const uint8_t rPinky = A6;
 
 const int pressureThreshold = 900;
 
@@ -13,15 +18,25 @@ void setup() {
 }
 
 void loop() {
-    Serial.print("thumb: ");
+    Serial.print("rThumb: ");
     Serial.print(analogRead(rThumb) > pressureThreshold ? "1" : "0");
-    Serial.print("\tindex: ");
+    Serial.print("\trIndex: ");
     Serial.print(analogRead(rIndex) > pressureThreshold ? "1" : "0");
-    Serial.print("\tmiddle: ");
+    Serial.print("\trMiddle: ");
     Serial.print(analogRead(rMiddle) > pressureThreshold ? "1" : "0");
-    Serial.print("\tring: ");
+    Serial.print("\trRing: ");
     Serial.print(analogRead(rRing) > pressureThreshold ? "1" : "0");
-    Serial.print("\tpinky: ");
+    Serial.print("\trPinky: ");
     Serial.print(analogRead(rPinky) > pressureThreshold ? "1" : "0");
+    Serial.print("\tlThumb: ");
+    Serial.print(analogRead(lThumb) > pressureThreshold ? "1" : "0");
+    Serial.print("\tlIndex: ");
+    Serial.print(analogRead(lIndex) > pressureThreshold ? "1" : "0");
+    Serial.print("\tlMiddle: ");
+    Serial.print(analogRead(lMiddle) > pressureThreshold ? "1" : "0");
+    Serial.print("\tlRing: ");
+    Serial.print(analogRead(lRing) > pressureThreshold ? "1" : "0");
+    Serial.print("\tlPinky: ");
+    Serial.print(analogRead(lPinky) > pressureThreshold ? "1" : "0");
     Serial.print("\n");
 }
